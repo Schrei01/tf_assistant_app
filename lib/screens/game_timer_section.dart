6ef2afe4cd8bd050afbe140f07d9f6af982df8a7
Alejandro Vargas <alejovarf@gmail.com>
2025-06-02 13:33:35 -0500

    Clean architecture

--- lib/screens/game_timer_section.dart
+++ lib/screens/game_timer_section.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tf_assistant_app/cronometro_controller.dart';
-import 'package:tf_assistant_app/cronometro_widget.dart';
+import 'package:tf_assistant_app/controllers/cronometro_controller.dart';
+import 'package:tf_assistant_app/widgets/cronometro_widget.dart';
 
 class GameTimerSection extends StatelessWidget {
   const GameTimerSection({
