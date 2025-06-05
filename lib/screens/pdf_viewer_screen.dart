2a8c0673c3c51309850610a119dd8f30e701b8f0
Alejandro Vargas <alejovarf@gmail.com>
2025-06-04 22:35:10 -0500

    Cambio de visor pdf

--- lib/screens/pdf_viewer_screen.dart
+++ lib/screens/pdf_viewer_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:flutter_pdfview/flutter_pdfview.dart';
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
+// import 'package:flutter/services.dart' show rootBundle;
+// import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:pdfrx/pdfrx.dart';
+// import 'dart:io';
+// import 'package:path_provider/path_provider.dart';
 
 class PdfViewerScreen extends StatefulWidget {
   final String assetPath;
@@ -19,31 +20,12 @@ class PdfViewerScreen extends StatefulWidget {
 }
 
 class _PdfViewerScreenState extends State<PdfViewerScreen> {
-  String? localPath;
-
-  @override
-  void initState() {
-    super.initState();
-    preparePdf();
-  }
-
-  Future<void> preparePdf() async {
-    final bytes = await rootBundle.load(widget.assetPath);
-    final dir = await getTemporaryDirectory();
-    final file = File("${dir.path}/temp.pdf");
-    await file.writeAsBytes(bytes.buffer.asUint8List(), flush: true);
-    setState(() {
-      localPath = file.path;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
-      body: localPath != null
-          ? PDFView(filePath: localPath!)
-          : const Center(child: CircularProgressIndicator()),
+      body: PdfViewer.asset(widget.assetPath)
     );
   }
 }
